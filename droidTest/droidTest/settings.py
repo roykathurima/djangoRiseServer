fee357274f09f90cc606374a7aebe3d8310f843d
roykathurima <kathurimaroy@gmail.com>
2020-07-20 11:50:03 +0300

    undid the heroku stuff because it did not work

--- droidTest/droidTest/settings.py
+++ droidTest/droidTest/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '41l)5nivf6^zc9+1*ns5u27g=3!&ef@qi(#i_%l)0@@qq92zq)'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'dcffdc3ec930.ngrok.io',
+    # 'riseup-droidtest.herokuapp.com',
+    'kathurimaroy.pythonanywhere.com',
     '127.0.0.1', 
     'localhost'
 ]
@@ -53,6 +54,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # The rest are the django defaults
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +134,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Heroku related stuff down here
+# PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+# STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
+
+# # Extra lookup directories for collectstatic to find static files
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
+
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
